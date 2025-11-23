88796dff6a38133bc4ef8a25e928c8a5f159057f
bdushime <bdushime47@gmail.com>
2025-11-23 15:10:28 +0200

    making file changes

---
+++ Node-js/server5.js
@@ -0,0 +1,19 @@
+// import fs from 'fs'
+
+// import http from 'http'
+
+// const PORT = 9000;
+
+
+// const server = http.createServer((req,res)=>{
+//    //GET
+//   if(req.url === '/api/tasks' && req.method === 'GET'){
+     
+//   }
+      
+// })
+
+
+// server.listen(PORT,()=>{
+//     console.log(`Server listening at ${PORT}`)
+// })
