2f47c925ea9d6e1015acc2f7148d853be8227bf4
bdushime <bdushime47@gmail.com>
2025-11-24 15:52:10 +0200

    making file changes

--- Node-js/server5.js
+++ Node-js/server5.js
@@ -52,7 +52,7 @@ const server = http.createServer((req,res)=>{
             res.end(JSON.stringify({message:'file not found'}))
              }
             res.writeHead(200,{'Content-Type':'application/json'});
-            res.end(JSON.stringify({message:'task added'},tasks))    
+            res.end(JSON.stringify({message:'task added',tasks}))    
              
         })
       
@@ -63,6 +63,98 @@ const server = http.createServer((req,res)=>{
    
    } 
 
+   //PUT
+   else if(req.url.startsWith('/api/tasks/') && req.method==='PUT'){
+    const id = parseInt(req.url.split('/')[3]);
+    let body='';
+    req.on('data',(chunk)=>{
+        body +=chunk.toString();
+    })
+
+    req.on('end',()=>{
+        let newTask = JSON.parse(body);
+        fs.readFile('./task.json',(err,data)=>{
+            if(err){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'file not found'}))
+            }
+            let tasks = JSON.parse(data);
+
+            let index=tasks.findIndex(t=>t.id === id);
+
+            if(index === -1){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'task not found'}))
+            }
+
+            tasks[index] = {...tasks[index],...newTask};
+
+            fs.writeFile('./task.json',JSON.stringify(tasks,null,2),(err)=>{
+                if(err){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'failed to add task'}))
+                }
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'task ',tasks}))
+            })
+        }) 
+         
+    })
+   }
+
+   //DELETE
+   else if(req.url.startsWith('/api/tasks/') && req.method === 'DELETE'){
+     let id = parseInt(req.url.split('/')[3]);
+
+     fs.readFile('./task.json',(err,data)=>{
+         if(err){
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:'file not found'}))
+         }
+         let tasks = JSON.parse(data);
+         let index = tasks.findIndex(t=>t.id=== id);
+
+         if(index === -1){
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:'task not found'}))
+         }
+
+         tasks.splice(index,1);
+
+         fs.writeFile('./task.json',JSON.stringify(tasks,null,2),(err)=>{
+            if(err){
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'task not deleted'}))
+            }
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:'task deleted',tasks}))
+         })
+     })
+   }
+
+   //GET BY ID
+
+   else if(req.url.startsWith('/api/tasks/') && req.method === 'GET'){
+    const id = parseInt(req.url.split('/')[3]);
+    fs.readFile('./task.json',(err,data)=>{
+        if(err){
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:'file not found'}))
+        }
+      
+        let tasks = JSON.parse(data);
+
+        let task = tasks.find(t=>t.id === id);
+
+        if(!task){
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:'task not found'}))
+        }
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({message:'task found',task}))
+        
+    })
+   }
 
       
 })
