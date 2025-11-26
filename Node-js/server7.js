8fcd70b9d3706664fe4fc5ed8aa22202f891ad55
bdushime <bdushime47@gmail.com>
2025-11-26 17:04:34 +0200

    express js revision

---
+++ Node-js/server7.js
@@ -0,0 +1,60 @@
+import http from 'http'
+import fs from 'fs'
+
+const PORT = 9001;
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/api/fab' && req.method=== 'GET'){
+        fs.readFile('./input.json','utf-8',(err,data)=>{
+            if(err){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'Error reading file'}))
+            } 
+
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.end(data);
+
+        })
+    }
+
+    else if(req.url.match('/api/fab') && req.method==='POST'){
+        let body='';
+
+        req.on('data',(chunk)=>{
+           body +=chunk.toString();
+        })
+
+        req.on('end',()=>{
+            let newTask = JSON.parse(body);
+
+            fs.readFile('./input.json',(err,data)=>{
+                if(err){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'Error reading file'}))
+            } 
+            
+            const tasks = JSON.parse(data);
+
+            tasks.push(newTask);
+            
+             fs.writeFile('./output.json',JSON.stringify(tasks,null,2),(err)=>{
+                if(err){
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'Error writing to file'}))
+                }
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'Task added successfully',tasks}));
+             })
+            })
+        })
+
+    }
+
+    
+})
+
+
+server.listen(PORT,()=>{
+        console.log(`Sever listening at port: ${PORT}`);
+    })
